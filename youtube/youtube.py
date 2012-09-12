a666c0982f197bfc8b5376029a1e84b9b6d4db54
Lorenzo Gil Sanchez <lorenzo.gil.sanchez@gmail.com>
2012-09-12 22:57:09 +0200

    Remove trailing whitespace

--- youtube/youtube.py
+++ youtube/youtube.py
@@ -295,10 +295,10 @@ def safe_filename(text, max_length=200):
     #Tidy up ugly formatted filenames.
     text = text.replace('_', ' ')
     text = text.replace(':', ' -')
-        
+
     #NTFS forbids filenames containing characters in range 0-31 (0x00-0x1F)
     ntfs = [chr(i) for i in range(0, 31)]
-    
+
     #Removing these SHOULD make most filename safe for a wide range
     #of operating systems.
     paranoid = ['\"', '\#', '\$', '\%', '\'', '\*', '\,', '\.', '\/', '\:',
