f763abce8a3a1b7b30f23ddd9e694c62cb5532f2
NFicano <NFicano@gmail.com>
2012-09-15 04:52:36 -0400

    fixed syntax error in setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     long_description=read('README.md'),
     classifiers=[
         "Development Status :: 4 - Beta",
-        "Environment :: Console"
+        "Environment :: Console",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 2.6",
