eb9f6eaeed6a2b6dd58183307f010d59cc0fa796
Nick Ficano <nick@keep.com>
2015-08-19 00:17:52 -0400

    bumped verison to 0.2.1

--- pytube/__init__.py
+++ pytube/__init__.py
@@ -1,8 +1,8 @@
 __title__ = 'pytube'
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 __author__ = 'Nick Ficano'
 __license__ = 'MIT License'
-__copyright__ = 'Copyright 2013 Nick Ficano'
+__copyright__ = 'Copyright 2015 Nick Ficano'
 
 from .api import YouTube
 YouTube
