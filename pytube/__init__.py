978cd9ceaa8429f1c64bce094b68f139bedd2a6d
Nick Ficano <nick@keep.com>
2015-09-26 22:03:29 -0400

    lets hold off on docs

--- pytube/__init__.py
+++ pytube/__init__.py
@@ -5,4 +5,14 @@ __license__ = 'MIT License'
 __copyright__ = 'Copyright 2015 Nick Ficano'
 
 from .api import YouTube
-YouTube
+
+# Set default logging handler to avoid "No handler found" warnings.
+import logging
+try:  # Python 2.7+
+    from logging import NullHandler
+except ImportError:
+    class NullHandler(logging.Handler):
+        def emit(self, record):
+            pass
+
+logging.getLogger(__name__).addHandler(NullHandler())
