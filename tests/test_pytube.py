59780206774274cc3c9be39eacdfef13e15dc0d3
Nick Ficano <nficano@gmail.com>
2015-10-04 22:27:05 -0400

    changes for nose test, added mock data

--- tests/test_pytube.py
+++ tests/test_pytube.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env/python
 # -*- coding: utf-8 -*-
 
-import unittest
+from nose.tools import eq_, raises
 from pytube import YouTube
 from pytube.exceptions import MultipleObjectsReturned
 
-SHOW_ERROR_MESSAGES = True
 
-
-class TestYouTube(unittest.TestCase):
+class TestYouTube(object):
     """Test all methods of Youtube class"""
 
     def setUp(self):
@@ -18,43 +16,30 @@ class TestYouTube(unittest.TestCase):
         self.video_id = 'Ik-RsDGPI5Y'
         self.filename = 'Pulp Fiction - Dancing Scene'
         self.yt = YouTube(self.url)
-        #: don't hard code, make is universal
-        self.videos = [
-            '<Video: MPEG-4 Visual (.3gp) - 144p - Simple>',
-            '<Video: MPEG-4 Visual (.3gp) - 240p - Simple>',
-            '<Video: Sorenson H.263 (.flv) - 240p - N/A>',
-            '<Video: H.264 (.mp4) - 360p - Baseline>',
-            '<Video: H.264 (.mp4) - 720p - High>',
-            '<Video: VP8 (.webm) - 360p - N/A>'
-        ]
         # using flv since it has only once video
         self.flv = '<Video: Sorenson H.263 (.flv) - 240p - N/A>'
 
     def test_url(self):
-        self.assertEqual(self.yt.url, self.url)
+        eq_(self.yt.url, self.url)
 
     def test_video_id(self):
-        self.assertEqual(self.yt.video_id, self.video_id)
+        eq_(self.yt.video_id, self.video_id)
 
     def test_filename(self):
-        self.assertEqual(self.yt.filename, self.filename)
+        eq_(self.yt.filename, self.filename)
 
     def test_get_videos(self):
-        self.assertEqual(map(str, self.yt.get_videos()), self.videos)
+        eq_(self.yt.get_videos())
 
     def test_get_video_data(self):
-        self.assertEqual((self.yt.get_video_data()['args']['loaderUrl']),
-                         self.url)
+        eq_((self.yt.get_video_data()['args']['loaderUrl']), self.url)
 
+    @raises(MultipleObjectsReturned)
     def test_get_false(self):
-        with self.assertRaises(MultipleObjectsReturned):
-            self.yt.get()
+        self.yt.get()
 
     def test_get_true(self):
-        self.assertEqual(str(self.yt.get('flv')), self.flv)
+        eq_(str(self.yt.get('flv')), self.flv)
 
     def test_filter(self):
-        self.assertEqual(str(self.yt.filter('flv')[0]), self.flv)
-
-if __name__ == '__main__':
-    unittest.main()
+        eq_(str(self.yt.filter('flv')[0]), self.flv)
