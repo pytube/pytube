6ef731031364b072797d28047bce8cb7435539be
Nick Ficano <nficano@gmail.com>
2015-10-05 22:40:21 -0400

    mocked all the html and js with local fixtures

--- tests/test_pytube.py
+++ tests/test_pytube.py
@@ -1,45 +1,22 @@
 #!/usr/bin/env/python
 # -*- coding: utf-8 -*-
 
-from nose.tools import eq_, raises
-from pytube import YouTube
-from pytube.exceptions import MultipleObjectsReturned
+import mock
+from nose.tools import eq_
+from pytube import api
 
 
-class TestYouTube(object):
-    """Test all methods of Youtube class"""
-
+class TestPytube(object):
     def setUp(self):
-        """Set up the all attributes required for a particular video."""
+        url = 'http://www.youtube.com/watch?v=Ik-RsDGPI5Y'
+        mock_js = open('tests/mock_video.js')
+        mock_html = open('tests/mock_video.html')
+        with mock.patch('pytube.api.urlopen') as urlopen:
+            urlopen.return_value.read.return_value = mock_html.read()
+            self.yt = api.YouTube()
+            self.yt._js_code = mock_js.read()
+            self.yt.from_url(url)
 
-        self.url = "https://www.youtube.com/watch?v=Ik-RsDGPI5Y"
-        self.video_id = 'Ik-RsDGPI5Y'
-        self.filename = 'Pulp Fiction - Dancing Scene'
-        self.yt = YouTube(self.url)
-        # using flv since it has only once video
-        self.flv = '<Video: Sorenson H.263 (.flv) - 240p - N/A>'
-
-    def test_url(self):
-        eq_(self.yt.url, self.url)
-
-    def test_video_id(self):
-        eq_(self.yt.video_id, self.video_id)
-
-    def test_filename(self):
-        eq_(self.yt.filename, self.filename)
-
-    def test_get_videos(self):
-        eq_(self.yt.get_videos())
-
-    def test_get_video_data(self):
-        eq_((self.yt.get_video_data()['args']['loaderUrl']), self.url)
-
-    @raises(MultipleObjectsReturned)
-    def test_get_false(self):
-        self.yt.get()
-
-    def test_get_true(self):
-        eq_(str(self.yt.get('flv')), self.flv)
-
-    def test_filter(self):
-        eq_(str(self.yt.filter('flv')[0]), self.flv)
+    def test_get_video_id(self):
+        """Resolve the video id from url"""
+        eq_(self.yt.video_id, 'Ik-RsDGPI5Y')
