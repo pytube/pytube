ff5c6f3e828cae21096d02899f9adb5e84916ed9
Nick Ficano <nficano@gmail.com>
2015-10-06 20:47:26 -0400

    remove 3.2 support

--- setup.py
+++ setup.py
@@ -31,7 +31,6 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.2",
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
