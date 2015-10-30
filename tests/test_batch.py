5f7668393d19025f90616cc9ef77cf2b31150c05
KeluDiao <diaokelu@gmail.com>
2015-10-30 17:46:47 -0400

    ...

--- tests/test_batch.py
+++ tests/test_batch.py
@@ -14,9 +14,9 @@ class TestBatch(object):
 	def __init__(self):
 		self.url = 'http://www.youtube.com/watch?v=C0DPdy98e4c'
 		self.video_id = "C0DPdy98e4c"
-		self.batch_download_file = './mock_data/test_output'
-		self.test_file = './mock_data/youtube_test_video.mp4'
-		self.merge_file = './mock_data/youtube_test_video_double'
+		self.batch_download_file = 'tests/mock_data/test_output'
+		self.test_file = 'tests/mock_data/youtube_test_video.mp4'
+		self.merge_file = 'tests/mock_data/youtube_test_video_double'
 		self.downloader = batch.Batch(path = self.batch_download_file)
 
 	def test_batch_download_by_id_list(self):
@@ -24,41 +24,25 @@ class TestBatch(object):
 		self.downloader.download_by_id_list(id_list)
 		eq_(filecmp.cmp(self.test_file, self.batch_download_file), True);
 		path = os.path.normpath(self.batch_download_file)
-		try:
-			os.remove(path)
-		except OSError as e:
-			print("Failed with:{0}".format(e.strerror))
-			print("Error code:{0}".format(e.code))
+		os.remove(path)
 
 	def test_batch_download_by_url_list(self):
 		url_list = [self.url]
 		self.downloader.download_by_url_list(url_list)
 		eq_(filecmp.cmp(self.test_file, self.batch_download_file), True);
 		path = os.path.normpath(self.batch_download_file)
-		try:
-			os.remove(path)
-		except OSError as e:
-			print("Failed with:{0}".format(e.strerror))
-			print("Error code:{0}".format(e.code))
+		os.remove(path)
 
 	def test_batch_merge_video_by_url_list(self):
 		url_list = [self.url, self.url]
 		self.downloader.merge_video_by_url_list(url_list)
 		eq_(filecmp.cmp(self.merge_file, self.batch_download_file), True);
 		path = os.path.normpath(self.batch_download_file)
-		try:
-			os.remove(path)
-		except OSError as e:
-			print("Failed with:{0}".format(e.strerror))
-			print("Error code:{0}".format(e.code))
+		os.remove(path)
 
 	def test_batch_merge_video_by_id_list(self):
 		id_list = [self.video_id, self.video_id]
 		self.downloader.merge_video_by_id_list(id_list)
 		eq_(filecmp.cmp(self.merge_file, self.batch_download_file), True);
 		path = os.path.normpath(self.batch_download_file)
-		try:
-			os.remove(path)
-		except OSError as e:
-			print("Failed with:{0}".format(e.strerror))
-			print("Error code:{0}".format(e.code))
+		os.remove(path)
