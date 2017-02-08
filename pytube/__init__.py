85e2df565efa8680ef1ee2b88a7aebcd65ac19a0
Nick Ficano <nficano@gmail.com>
2017-02-08 14:48:20 -0500

    Release version 6.2.2

--- pytube/__init__.py
+++ pytube/__init__.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # flake8: noqa
 __title__ = 'pytube'
-__version__ = '6.2.1'
+__version__ = '6.2.2'
 __author__ = 'Nick Ficano'
 __license__ = 'MIT License'
 __copyright__ = 'Copyright 2017 Nick Ficano'
