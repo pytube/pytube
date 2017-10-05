3c080b4b1f959f1e7bd0c21d38946c982a0646dc
Nick Ficano <nficano@gmail.com>
2017-10-04 20:05:57 -0400

    __init__ info

--- pytube/__init__.py
+++ pytube/__init__.py
@@ -9,6 +9,12 @@ A lightweight, dependency-free Python library (and command-line utility) for
 downloading YouTube Videos. It's extensively documented and follows best
 practice patterns.
 """
+__title__ = 'pytube'
+__version__ = '6.4.3'
+__author__ = 'Nick Ficano'
+__license__ = 'MIT License'
+__copyright__ = 'Copyright 2017 Nick Ficano'
+
 from pytube.query import StreamQuery
 from pytube.streams import Stream
 from pytube.__main__ import YouTube
