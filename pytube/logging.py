4ed36577f4b284e25e0a52482e3f8603508b1687
Nick Ficano <nficano@gmail.com>
2017-10-05 14:00:37 -0400

    cleaned up logging

--- pytube/logging.py
+++ pytube/logging.py
@@ -10,10 +10,12 @@ import logging
 
 def create_logger(level=logging.DEBUG):
     fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logger = logging.getLogger()
-    handler = logging.StreamHandler()
     formatter = logging.Formatter(fmt)
+
+    handler = logging.StreamHandler()
     handler.setFormatter(formatter)
+
+    logger = logging.getLogger()
     logger.addHandler(handler)
     logger.setLevel(level)
     return logger
