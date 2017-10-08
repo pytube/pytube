fac0e7dc8e570bf8a3f14131672a8d06b47aa997
Nick Ficano <nficano@gmail.com>
2017-10-07 22:50:59 -0400

    cli docstrings

--- pytube/cli.py
+++ pytube/cli.py
@@ -40,36 +40,61 @@ def main():
         download(args.url, args.itag)
 
 
-def _get_terminal_size():
+def get_terminal_size():
     """Returns the terminal size in rows and columns."""
     rows, columns = os.popen('stty size', 'r').read().split()
     return int(rows), int(columns)
 
 
-def _render_progress_bar(bar, percent):
+def render_progress_percentage(bar, percent):
+    """Writes the updated progress bar percentage and filled area.
+
+    :param str bar:
+        The filled and unfilled remainder chars of the progress bar.
+    :param int percent:
+        Numerical representation of progress completion.
+    """
     text = ' ↳ |{bar}| {percent}%\r'.format(bar=bar, percent=percent)
     sys.stdout.write(text)
     sys.stdout.flush()
 
 
 def display_progress_bar(count, total, ch='█', scale=0.55):
-    _, columns = _get_terminal_size()
+    _, columns = get_terminal_size()
     max_width = int(columns * scale)
 
     filled = int(round(max_width * count / float(total)))
     remaining = max_width - filled
     bar = ch * filled + ' ' * remaining
     percent = round(100.0 * count / float(total), 1)
-    _render_progress_bar(bar, percent)
+    render_progress_percentage(bar, percent)
 
 
 def on_progress(stream, file_handle, bytes_remaining):
+    """On download progress callback function.
+
+    :param object stream:
+        An instance of :class:`Stream <Stream>` being downloaded.
+    :param :class:`_io.BufferedWriter <BufferedWriter>` file_handle:
+        The file handle where the media is being written to.
+    :param int bytes_remaining:
+        The delta between the total file size in bytes and amount already
+        downloaded.
+    """
     total = stream.filesize
     count = total - bytes_remaining
     display_progress_bar(count, total)
 
 
 def download(url, itag):
+    """Begin downloading a YouTube video.
+
+    :param str url:
+        A valid YouTube watch URL.
+    :param str itag:
+        YouTube format identifier code.
+
+    """
     # TODO(nficano): allow download target to be specified
     # TODO(nficano): allow dash itags to be selected
     yt = YouTube(url, on_progress_callback=on_progress)
@@ -83,6 +108,11 @@ def download(url, itag):
 
 
 def display_streams(url):
+    """Probes YouTube video and lists its available formats.
+
+    :param str url:
+        A valid YouTube watch URL.
+    """
     yt = YouTube(url)
     for stream in yt.streams.all():
         print(stream)
