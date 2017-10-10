e83045b8e9174569e1efe774ca560749fd1165aa
Nick Ficano <nficano@gmail.com>
2017-10-10 09:44:52 -0400

    itag test coverage

---
+++ tests/test_itags.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from pytube import itags
+
+
+def test_get_format_profile():
+    profile = itags.get_format_profile(22)
+    assert profile['resolution'] == '720p'
+
+
+def test_get_format_profile_non_existant():
+    profile = itags.get_format_profile(2239)
+    assert profile['resolution'] is None
