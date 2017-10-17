9ac7fcd51af357e79c9e4f7c970f072d725b5a01
Nick Ficano <nficano@gmail.com>
2017-10-17 14:34:54 -0400

    Bump version: 7.0.10 → 7.0.11

--- pytube/__init__.py
+++ pytube/__init__.py
@@ -9,7 +9,7 @@ follows best practice patterns.
 
 """
 __title__ = 'pytube'
-__version__ = '7.0.10'
+__version__ = '7.0.11'
 __author__ = 'Nick Ficano'
 __license__ = 'MIT License'
 __copyright__ = 'Copyright 2017 Nick Ficano'
