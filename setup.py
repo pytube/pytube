89e78b25d884d5ee68825f5b78c6b24aa99aea2a
Nick Ficano <nficano@gmail.com>
2017-10-23 17:40:32 -0400

    Bump version: 7.0.14 → 7.0.15

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open('LICENSE') as readme_file:
 
 setup(
     name='pytube',
-    version='7.0.14',
+    version='7.0.15',
     author='Nick Ficano',
     author_email='nficano@gmail.com',
     packages=['pytube'],
