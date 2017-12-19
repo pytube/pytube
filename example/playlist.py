dfdaa3526bb13b5cef939f7239f3716749ec1231
AGENT47 <thomasscms@gmail.com>
2017-12-19 15:58:34 +0530

    Update playlist.py
    fixed flake errors

--- example/playlist.py
+++ example/playlist.py
@@ -4,6 +4,7 @@ import urllib2
 from bs4 import BeautifulSoup as bs
 from pytube import YouTube
 import re
+import argparse
 
 
 def playlist(url):
@@ -17,16 +18,23 @@ def playlist(url):
         files_in_pl.append('https://www.youtube.com' + vid_id)
 
     for link in files_in_pl:
+        print 'downloading ' + YouTube(link).title
         YouTube(link).streams.first().download()
 
 
 if __name__ == '__main__':
-    inurl = raw_input('Enter playlist url : ')
+    parser = argparse.ArgumentParser()
+    parser.add_argument("inp", type=str,
+                        help="youtuble playlist url")
+    args = parser.parse_args()
+    inurl = args.inp
     ret = re.search('com(.+?)\?', inurl)
     if(ret is not None):
         ret = ret.group(1)
     else:
+        print ('wrong url')
         quit()
     if(ret != '/playlist'):
+        print ('no url')
         quit()
     playlist(inurl)
