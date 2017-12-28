bdfa1a425c02190a19b9f013cb5d49e1790e6e5b
family-guy <guy@zorncapital.com>
2017-12-28 14:22:00 +0000

    Remove age restricted exception

--- pytube/exceptions.py
+++ pytube/exceptions.py
@@ -34,7 +34,3 @@ class ExtractError(PytubeError):
 
 class RegexMatchError(ExtractError):
     """Regex pattern did not return any matches."""
-
-
-class AgeRestrictionError(ExtractError):
-    """Content is age restricted."""
