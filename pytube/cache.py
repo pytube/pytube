5bc4c325e3b55fdac2fd495c742f59772662f629
Bill Davis <bill.davis@vertical-knowledge.com>
2018-03-02 13:29:35 -0500

    Flake8 line issue fix

--- pytube/cache.py
+++ pytube/cache.py
@@ -4,6 +4,7 @@ from functools import lru_cache
 
 from pytube import request
 
+
 @lru_cache(maxsize=None)
 def get_js(js_url):
     """Retrieve Javascript from js_url if Javascript is not already cached.
