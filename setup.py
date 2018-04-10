343e9a665994e98fe546aaea709053b97cac3d2d
Nick Ficano <nficano@gmail.com>
2018-04-10 10:03:58 -0400

    Bump version: 9.1.1 → 9.2.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open('LICENSE') as readme_file:
 
 setup(
     name='pytube',
-    version='9.1.1',
+    version='9.2.0',
     author='Nick Ficano',
     author_email='nficano@gmail.com',
     packages=['pytube', 'pytube.contrib'],
