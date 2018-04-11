56cf12dbf1955778cce872c53c8643ffba4b57f4
Bill Davis <bill.davis@vertical-knowledge.com>
2018-04-11 13:50:41 -0400

    Catch unavailable videos.

--- pytube/__main__.py
+++ pytube/__main__.py
@@ -21,6 +21,7 @@ from pytube import Stream
 from pytube import StreamQuery
 from pytube.compat import install_proxy
 from pytube.compat import parse_qsl
+from pytube.exceptions import VideoUnavailable
 from pytube.helpers import apply_mixin
 
 logger = logging.getLogger(__name__)
@@ -156,6 +157,8 @@ class YouTube(object):
 
         """
         self.watch_html = request.get(url=self.watch_url)
+        if '<img class="icon meh" src="/yts/img' not in self.watch_html:
+            raise VideoUnavailable('This video is unplayable.')
         self.embed_html = request.get(url=self.embed_url)
         self.age_restricted = extract.is_age_restricted(self.watch_html)
         self.vid_info_url = extract.video_info_url(
