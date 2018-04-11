b7c602b7ec26055d6c40d8f29fd43bc7a5e9dbd7
Bill Davis <bill.davis@vertical-knowledge.com>
2018-04-11 13:53:46 -0400

    Update exception message.

--- pytube/__main__.py
+++ pytube/__main__.py
@@ -158,7 +158,7 @@ class YouTube(object):
         """
         self.watch_html = request.get(url=self.watch_url)
         if '<img class="icon meh" src="/yts/img' not in self.watch_html:
-            raise VideoUnavailable('This video is unplayable.')
+            raise VideoUnavailable('This video is unavailable.')
         self.embed_html = request.get(url=self.embed_url)
         self.age_restricted = extract.is_age_restricted(self.watch_html)
         self.vid_info_url = extract.video_info_url(
