856cbee97d91b2b047c77cc51f742b7e47e00c7e
Nick Ficano <nficano@gmail.com>
2018-10-18 19:27:16 -0400

    Bump version: 9.3.3 → 9.3.4

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ class UploadCommand(Command):
 
 setup(
     name='pytube',
-    version='9.3.3',
+    version='9.3.4',
     author='Nick Ficano',
     author_email='nficano@gmail.com',
     packages=['pytube', 'pytube.contrib'],
