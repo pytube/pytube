52c2ada98b1d7b1c8bd772dff1404c926478f0eb
tenyearscoder <10yearscoder@gmail.com>
2019-03-05 14:01:02 +0900

    travisCI mod - too long url

--- tests/contrib/test_playlist.py
+++ tests/contrib/test_playlist.py
@@ -2,7 +2,8 @@ from pytube import Playlist
 
 
 def test_title():
-    url = "https://www.youtube.com/playlist?list=PLsyeobzWxl7poL9JTVyndKe62ieoN-MZ3"
+    list_key = "PLsyeobzWxl7poL9JTVyndKe62ieoN-MZ3"
+    url = "https://www.youtube.com/playlist?list=" + list_key
     pl = Playlist(url)
     pl_title = pl.title()
     assert pl_title == "Python Tutorial for Beginners"
