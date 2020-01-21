9b3e15fbfd300a7bd43e1d18d811bacf7c6e2461
Harold Martin <harold.martin@gmail.com>
2020-01-21 05:59:42 -0800

    add 3.8 to setup

--- setup.py
+++ setup.py
@@ -37,6 +37,7 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python",
         "Topic :: Internet",
         "Topic :: Multimedia :: Video",
