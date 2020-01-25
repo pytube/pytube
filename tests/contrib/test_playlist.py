86b6955f5e88566b169bbd6549d3a7cda89e6b86
Harold Martin <harold.martin@gmail.com>
2020-01-25 10:08:38 -0800

    fix test_playlist.test_title

--- tests/contrib/test_playlist.py
+++ tests/contrib/test_playlist.py
@@ -1,10 +1,16 @@
 # -*- coding: utf-8 -*-
+from unittest import mock
+
 from pytube import Playlist
 
 
-def test_title():
-    list_key = "PLsyeobzWxl7poL9JTVyndKe62ieoN-MZ3"
-    url = "https://www.youtube.com/playlist?list=" + list_key
+@mock.patch("pytube.contrib.playlist.request.get")
+def test_title(request_get):
+    request_get.return_value = (
+        "<title>(149) Python Tutorial for Beginners "
+        "(For Absolute Beginners) - YouTube</title>"
+    )
+    url = "https://www.fakeurl.com/playlist?list=PLsyeobzWxl7poL9JTVyndKe62ieoN"
     pl = Playlist(url)
     pl_title = pl.title()
-    assert pl_title == "Python Tutorial for Beginners"
+    assert pl_title == "(149) Python Tutorial for Beginners (For Absolute Beginners)"
