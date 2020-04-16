8ea02d377bf0103069c509fed62b06bf5bbf1ab0
tooxo <till@s.chulte.de>
2020-04-16 16:17:33 +0200

    fix tests

--- pytube/extract.py
+++ pytube/extract.py
@@ -278,9 +278,10 @@ def apply_descrambler(stream_data: Dict, key: str) -> None:
     if key == "url_encoded_fmt_stream_map" and not stream_data.get(
         "url_encoded_fmt_stream_map"
     ):
-        formats: List[Dict[str, Union[str, int, float]]] = []
-        player_response: Dict[str, Union[Dict[str, dict], List[dict], str]] = \
-            json.loads(stream_data["player_response"])
+        formats: List[Dict[str, str]] = []
+        player_response: Dict[str, Union[Dict[str, Union[list, dict]]]] = json.loads(
+            stream_data["player_response"]
+        )
         if "formats" in player_response["streamingData"]:
             formats.extend(player_response["streamingData"]["formats"])
         if "adaptiveFormats" in player_response["streamingData"]:
