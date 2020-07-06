35c03351b90eedc2d3c419a5a9168022165f8a91
rsolanoweb <rubensoljim@gmail.com>
2020-07-06 16:28:00 +0200

    unnecessary code

--- tests/contrib/test_playlist.py
+++ tests/contrib/test_playlist.py
@@ -8,10 +8,6 @@ from pytube import Playlist
 
 @mock.patch("pytube.contrib.playlist.request.get")
 def test_title(request_get):
-    request_get.html = (
-        "<title>YouTube</title> <title>(149) Python Tutorial for Beginners "
-        "(For Absolute Beginners) - YouTube</title>"
-    )
     request_get.return_value = (
         "<title>(149) Python Tutorial for Beginners "
         "(For Absolute Beginners) - YouTube</title>"
