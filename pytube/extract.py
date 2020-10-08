145af4247915a6b29f477ed91c75e407dccb5c61
Alexander Czyrny <mail@czyrny.net>
2020-10-08 09:01:41 +0200

    Revert "Update extract.py"
    This reverts commit 57aaf20f625213eb195443bba49820e4626f71bf.

--- pytube/extract.py
+++ pytube/extract.py
@@ -217,7 +217,6 @@ def get_ytplayer_config(html: str) -> Any:
         Substring of the html containing the encoded manifest data.
     """
     config_patterns = [
-        r";ytplayer\.config\s*=\s*({.+?});ytplayer",
         r";ytplayer\.config\s*=\s*({.*?});\(function"
         r";ytplayer\.config\s*=\s*({.*?});",
         r";yt\.setConfig\(\{'PLAYER_CONFIG':\s*(\{.*})}\);",
