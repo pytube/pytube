bd0996ade6625a04f5b0b786e71950090863f13d
Taylor Fox Dahlin <9667869+tfdahlin@users.noreply.github.com>
2020-11-13 10:58:21 -0500

    Added python 3.9 to classifiers.

--- setup.py
+++ setup.py
@@ -40,6 +40,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python",
         "Topic :: Internet",
         "Topic :: Multimedia :: Video",
