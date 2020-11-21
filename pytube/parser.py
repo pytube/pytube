bd1408f03d1ca77038e214ba2e36bf1bc7bbc8f0
Taylor Fox Dahlin <9667869+tfdahlin@users.noreply.github.com>
2020-11-21 14:56:38 -0500

    Added a parser for javascript objects within html.

---
+++ pytube/parser.py
@@ -0,0 +1,88 @@
+import ast
+import json
+import re
+from pytube.exceptions import HTMLParseError
+
+def parse_for_object(html, preceding_regex):
+    """Parses input html to find the end of a JavaScript object.
+
+    :param str html:
+        HTML to be parsed for an object.
+    :param str preceding_regex:
+        Regex to find the string preceding the object.
+    :rtype dict:
+    :returns:
+        A dict created from parsing the object.
+    """
+    regex = re.compile(preceding_regex)
+    result = regex.search(html)
+    if not result:
+        raise HTMLParseError(f'No matches for regex {preceding_regex}')
+
+    start_index = result.span()[1]
+    return parse_for_object_from_startpoint(html, start_index)
+
+
+def parse_for_object_from_startpoint(html, start_point):
+    """Parses input html to find the end of a JavaScript object.
+
+    :param str html:
+        HTML to be parsed for an object.
+    :param int start_point:
+        Index of where the object starts.
+    :rtype dict:
+    :returns:
+        A dict created from parsing the object.
+    """
+    html = html[start_point:]
+    if html[0] != '{':
+        raise HTMLParseError('Invalid start point.')
+
+    # First letter MUST be a open brace, so we put that in the stack, and skip the first character.
+    stack = ['{']
+    i = 1
+
+    context_openers = [
+        '{',
+        '[',
+        '"'
+    ]
+
+    context_closers = {
+        '{': '}',
+        '[': ']',
+        '"': '"'
+    }
+
+    while i < len(html):
+        if len(stack) == 0:
+            break
+        curr_char = html[i]
+        curr_context = stack[-1]
+
+        # If we've reached a context closer, we can remove an element off the stack
+        if curr_char == context_closers[curr_context]:
+            stack.pop()
+            i += 1
+            continue
+
+        # Strings require special context handling because they can contain
+        #  context openers *and* closers
+        if curr_context == '"':
+            # If there's a backslash in a string, we skip a character
+            if curr_char == '\\':
+                next_char = html[i+1]
+                i += 2
+                continue
+        else:
+            # Non-string contexts are when we need to look for context openers.
+            if curr_char in context_closers.keys():
+                stack.append(curr_char)
+
+        i += 1
+
+    full_obj = html[:i]
+    try:
+        return json.loads(full_obj)
+    except:
+        return ast.literal_eval(full_obj)
---
+++ tests/test_parser.py
@@ -0,0 +1,47 @@
+import pytest
+from unittest import mock
+
+from pytube.exceptions import HTMLParseError
+from pytube.parser import parse_for_object
+
+
+def test_invalid_start():
+    with pytest.raises(HTMLParseError):
+        parse_for_object('test = {}', r'invalid_regex')
+
+
+def test_parse_simple_empty_object():
+    result = parse_for_object('test = {}', r'test\s*=\s*')
+    assert result == {}
+
+
+def test_parse_longer_empty_object():
+    test_html = """test = {
+
+
+    }"""
+    result = parse_for_object(test_html, r'test\s*=\s*')
+    assert result == {}
+
+
+def test_parse_empty_object_with_trailing_characters():
+    test_html = 'test = {};'
+    result = parse_for_object(test_html, r'test\s*=\s*')
+    assert result == {}
+
+
+def test_parse_simple_object():
+    test_html = 'test = {"foo": [], "bar": {}};'
+    result = parse_for_object(test_html, r'test\s*=\s*')
+    assert result == {
+        'foo': [],
+        'bar': {}
+    }
+
+
+def test_parse_context_closer_in_string_value():
+    test_html = 'test = {"foo": "};"};'
+    result = parse_for_object(test_html, r'test\s*=\s*')
+    assert result == {
+        'foo': '};'
+    }
