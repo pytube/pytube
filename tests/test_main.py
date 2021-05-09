edab11d29c10e0b1e23f45ea864745b7ad31c902
Taylor Fox Dahlin <9667869+tfdahlin@users.noreply.github.com>
2021-05-09 16:49:32 -0400

    Removed irrelevant test.

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,14 +7,6 @@ from pytube import YouTube
 from pytube.exceptions import RegexMatchError
 
 
-@mock.patch("pytube.__main__.YouTube")
-def test_prefetch_deferred(youtube):
-    instance = youtube.return_value
-    instance.prefetch_descramble.return_value = None
-    YouTube("https://www.youtube.com/watch?v=9bZkp7q19f0", True)
-    assert not instance.prefetch_descramble.called
-
-
 @mock.patch("urllib.request.install_opener")
 def test_install_proxy(opener):
     proxies = {"http": "http://www.example.com:3128/"}
