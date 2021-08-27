47aa4177232cdd7f74591e10437a49b8580d0c3b
Taylor Fox Dahlin <9667869+tfdahlin@users.noreply.github.com>
2021-08-27 18:44:27 -0400

    The variable being changed is 'n', so we should explicitly reference it to prevent mismatches.

--- pytube/cipher.py
+++ pytube/cipher.py
@@ -265,7 +265,7 @@ def get_throttling_function_name(js: str) -> str:
         # https://github.com/ytdl-org/youtube-dl/issues/29326#issuecomment-865985377
         # a.C&&(b=a.get("n"))&&(b=Dea(b),a.set("n",b))}};
         # In above case, `Dea` is the relevant function name
-        r'a\.[A-Z]&&\(b=a\.[sg]et\([^\)]+\)\)&&\(b=([^(]+)\(b\)',
+        r'a\.[A-Z]&&\(b=a\.[sg]et\("n"\)\)&&\(b=([^(]+)\(b\)',
     ]
     logger.debug('Finding throttling function name')
     for pattern in function_patterns:
