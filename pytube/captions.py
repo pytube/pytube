2ec4fd7da667c35ca7aba527c4a4ae30f08c8448
ladm99 <ladelmar99@gmail.com>
2022-01-05 00:47:25 -0600

    Fixes pytube#1077

--- pytube/captions.py
+++ pytube/captions.py
@@ -60,7 +60,7 @@ class Caption:
 
         float_to_srt_time_format(3.89) -> '00:00:03,890'
         """
-        fraction, whole = math.modf(d)
+        fraction, whole = math.modf(d/1000)
         time_fmt = time.strftime("%H:%M:%S,", time.gmtime(whole))
         ms = f"{fraction:.3f}".replace("0.", "")
         return time_fmt + ms
@@ -73,14 +73,16 @@ class Caption:
         """
         segments = []
         root = ElementTree.fromstring(xml_captions)
-        for i, child in enumerate(list(root)):
-            text = child.text or ""
+        for i, child in enumerate(list(root.findall('body/p'))):
+            text = ''.join(child.itertext()).strip()
+            if not text:
+                continue
             caption = unescape(text.replace("\n", " ").replace("  ", " "),)
             try:
-                duration = float(child.attrib["dur"])
+                duration = float(child.attrib["d"])
             except KeyError:
                 duration = 0.0
-            start = float(child.attrib["start"])
+            start = float(child.attrib["t"])
             end = start + duration
             sequence_number = i + 1  # convert from 0-indexed to 1.
             line = "{seq}\n{start} --> {end}\n{text}\n".format(
