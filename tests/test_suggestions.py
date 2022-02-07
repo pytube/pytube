108dc012beff1d53a63174e22c0b68ad6106cfb6
nik1097 <nikhilsuresh097@gmail.com>
2022-02-07 12:15:36 -0500

    added unit test file to verify changes made

---
+++ tests/test_suggestions.py
@@ -0,0 +1,16 @@
+from pytube import Search
+#might have to handle new objects that might be created by YouTube in the future
+def test_completion_suggestions():
+    temp = "#crapsnation" #handled missing hashtagTileRenderer object
+    s = Search(temp)
+    assert s.completion_suggestions
+
+    temp_1 = "No Time To Die Billie Eilish" #handled missing movieRenderer object
+    s = Search(temp_1)
+    assert s.completion_suggestions
+
+    temp_2 = "rangmnam style" #handled missing showingResultsForRenderer object
+    s = Search(temp_2)
+    assert s.completion_suggestions
+
+test_completion_suggestions()
