daf5c4b2fe3747f2ed258b7e02b5b8834c4ac77d
nik1097 <nikhilsuresh097@gmail.com>
2022-02-10 19:39:02 -0500

    updated the test file with better error checks

--- tests/contrib/test_search.py
+++ tests/contrib/test_search.py
@@ -1,16 +1,36 @@
 from pytube import Search
-#  might have to handle new objects that might be created by YouTube in the future
-def test_completion_suggestions():
-    temp = "#crapsnation" #  handled missing hashtagTileRenderer object
-    s = Search(temp)
-    assert s.completion_suggestions
 
-    temp_1 = "No Time To Die Billie Eilish" #  handled missing movieRenderer object
-    s = Search(temp_1)
-    assert s.completion_suggestions
 
-    temp_2 = "rangmnam style" #  handled missing showingResultsForRenderer object
-    s = Search(temp_2)
-    assert s.completion_suggestions
+def test_for_completion_suggestions():
 
-test_completion_suggestions()
+    temp_0 = "#crapsnation"  # check missing hashtagTileRenderer object
+    try:
+        s = Search(temp_0)
+        if 'Unexpected renderer encountered.' not in s.completion_suggestions:
+            assert True
+    except KeyError:
+        assert False
+
+    temp = "crapsnation"  # check for words without suggestions
+    try:
+        s = Search(temp)
+        if 'Unexpected renderer encountered.' not in s.completion_suggestions:
+            assert True
+    except KeyError:
+        assert False
+
+    temp_1 = "gone with the wind"  # check missing movieRenderer object
+    try:
+        s = Search(temp_1)
+        if 'Unexpected renderer encountered.' not in s.completion_suggestions:
+            assert True
+    except KeyError:
+        assert False
+
+    temp_2 = "rangmnam style"  # check missing showingResultsForRenderer object
+    try:
+        s = Search(temp_2)
+        if 'Unexpected renderer encountered.' not in s.completion_suggestions:
+            assert True
+    except KeyError:
+        assert False
