b2d49055faa0458b0d839acce6a09228dbe05524
nik1097 <nikhilsuresh097@gmail.com>
2022-04-05 22:06:57 -0400

    used mocks for creating test cases for search

--- tests/contrib/test_search.py
+++ tests/contrib/test_search.py
@@ -1,27 +1,32 @@
+from unittest import mock
 from pytube import Search
-import unittest
 
-class TestCompletionSuggestions(unittest.TestCase):
-    def test_for_missing_hashtagTileRenderer(self): 
-        s = Search("#crapsnation") # check missing hashtagTileRenderer object
-        self.assertTrue(s.completion_suggestions, 'No suggestions available')
-    
-    def test_for_no_suggestions(self):
-        s = Search("crapsnation") # check for words without suggestions
-        self.assertTrue(s.completion_suggestions, 'No suggestions available')
+@mock.patch('pytube.request.get')
+def test_for_missing_hashtagTileRenderer(request_get, search_suggestion):
+    request_get.return_value = search_suggestion
+    s = Search("#crapsnation")
+    assert s.completion_suggestions == 'No suggestions available'
 
-    def test_for_missing_movieRenderer(self):
-        s = Search("gone with the wind") # check missing movieRenderer object
-        self.assertTrue(s.completion_suggestions, 'No suggestions available')
+@mock.patch('pytube.request.get')
+def test_test_for_no_suggestions(request_get, search_suggestion):
+    request_get.return_value = search_suggestion
+    s = Search("crapsnation")
+    assert s.completion_suggestions == 'No suggestions available'
 
-    def test_for_showingResultsForRenderer(self):
-        s = Search("rangmnam style") # check missing showingResultsForRenderer object
-        res = ['gangnam style just dance', 'gangnam style parody', 'gangnam style fortnite',
-                'gangnam style minecraft', 'gangnam style remix', 'gangnam style clean', 
-                'gangnam style reaction', 'gangnam style live', 'psy gentleman',
-                'gangnam style dance', 'gangnam style 1 hour', 'psy daddy',
-                'gangnam style lyrics', 'psy new face']
-        self.assertTrue(s.completion_suggestions, res)
+@mock.patch('pytube.request.get')
+def test_for_missing_movieRenderer(request_get, search_suggestion):
+    request_get.return_value = search_suggestion
+    s = Search("gone with the wind")
+    assert s.completion_suggestions == 'No suggestions available'
 
-if __name__ == '__main__':
-    unittest.main()
+@mock.patch('pytube.request.get')
+def test_for_showingResultsForRenderer(request_get, search_suggestion):
+    request_get.return_value = search_suggestion
+    s = Search("rangmnam style")
+    assert s.completion_suggestions == [
+        'gangnam style just dance', 'gangnam style parody', 'gangnam style fortnite',
+        'gangnam style minecraft', 'gangnam style remix', 'gangnam style clean', 
+        'gangnam style reaction', 'gangnam style live', 'psy gentleman',
+        'gangnam style dance', 'gangnam style 1 hour', 'psy daddy',
+        'gangnam style lyrics', 'psy new face'
+    ]
