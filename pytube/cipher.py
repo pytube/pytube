8667ab8a7de466aeff8545e8bd3e8b91589bd965
IntenseSoftwares <93078859+ParijatYT@users.noreply.github.com>
2022-04-15 11:09:32 +0530

    Update cipher.py
    Updated nfunc to:
    
    nfunc=re.escape(function_match.group(1))),

--- pytube/cipher.py
+++ pytube/cipher.py
@@ -262,13 +262,7 @@ def get_throttling_function_name(js: str) -> str:
         The name of the function used to compute the throttling parameter.
     """
     function_patterns = [
-        # https://github.com/ytdl-org/youtube-dl/issues/29326#issuecomment-865985377
-        # https://github.com/yt-dlp/yt-dlp/commit/48416bc4a8f1d5ff07d5977659cb8ece7640dcd8
-        # var Bpa = [iha];
-        # ...
-        # a.C && (b = a.get("n")) && (b = Bpa[0](b), a.set("n", b),
-        # Bpa.length || iha("")) }};
-        # In the above case, `iha` is the relevant function name
+
         r'a\.[a-zA-Z]\s*&&\s*\([a-z]\s*=\s*a\.get\("n"\)\)\s*&&\s*'
         r'\([a-z]\s*=\s*([a-zA-Z0-9$]{3})(\[\d+\])?\([a-z]\)',
     ]
@@ -285,7 +279,7 @@ def get_throttling_function_name(js: str) -> str:
                 idx = idx.strip("[]")
                 array = re.search(
                     r'var {nfunc}\s*=\s*(\[.+?\]);'.format(
-                        nfunc=function_match.group(1)),
+                        nfunc=re.escape(function_match.group(1))),
                     js
                 )
                 if array:
