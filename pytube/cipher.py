aa52ef84040e9d0ea2fde6fbf2bc0e731a94db7e
AmmarFarid10 <91678027+AmmarFarid10@users.noreply.github.com>
2022-04-19 10:57:12 +0500

    Line number 288 --> nfunc=re.escape(function_match.group(1))),

--- pytube/cipher.py
+++ pytube/cipher.py
@@ -285,7 +285,7 @@ def get_throttling_function_name(js: str) -> str:
                 idx = idx.strip("[]")
                 array = re.search(
                     r'var {nfunc}\s*=\s*(\[.+?\]);'.format(
-                        nfunc=function_match.group(1)),
+                        nfunc=re.escape(function_match.group(1))),
                     js
                 )
                 if array:
