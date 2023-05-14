44b705e53e455b975107d0198bc2aa555ad7fad2
Alexey Pelykh <alexey.pelykh@gmail.com>
2023-05-14 12:13:06 +0200

    Refactor request.stream()

--- tests/test_request.py
+++ tests/test_request.py
@@ -19,10 +19,12 @@ def test_streaming(mock_urlopen):
     ]
     mock_response = mock.Mock()
     mock_response.read.side_effect = fake_stream_binary
-    mock_response.info.return_value = {"Content-Range": "bytes 200-1000/24576"}
+    mock_response.info.return_value = {
+        "Content-Length": f"{3 * 8 * 1024}",
+    }
     mock_urlopen.return_value = mock_response
     # When
-    response = request.stream("http://fakeassurl.gov/streaming_test")
+    response = request.stream("http://example.com/streaming_test")
     # Then
     assert len(b''.join(response)) == 3 * 8 * 1024
     assert mock_response.read.call_count == 4
@@ -32,7 +34,7 @@ def test_streaming(mock_urlopen):
 def test_timeout(mock_urlopen):
     exc = URLError(reason=socket.timeout('timed_out'))
     mock_urlopen.side_effect = exc
-    generator = request.stream('http://fakeassurl.gov/timeout_test', timeout=1)
+    generator = request.stream('http://example.com/timeout_test', timeout=1)
     with pytest.raises(MaxRetriesExceeded):
         next(generator)
 
@@ -42,7 +44,7 @@ def test_headers(mock_urlopen):
     response = mock.Mock()
     response.info.return_value = {"content-length": "16384"}
     mock_urlopen.return_value = response
-    response = request.head("http://fakeassurl.gov")
+    response = request.head("http://example.com")
     assert response == {"content-length": "16384"}
 
 
@@ -51,7 +53,7 @@ def test_get(mock_urlopen):
     response = mock.Mock()
     response.read.return_value = "<html></html>".encode("utf-8")
     mock_urlopen.return_value = response
-    response = request.get("http://fakeassurl.gov")
+    response = request.get("http://example.com")
     assert response == "<html></html>"
 
 
