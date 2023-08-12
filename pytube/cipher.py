4f597b249e33963fcc77c677c6c229cbe6ee146f
SB5 TV <oncename@gmail.com>
2023-08-12 15:05:05 +0300

    Update cipher.py

--- pytube/cipher.py
+++ pytube/cipher.py
@@ -27,7 +27,7 @@ logger = logging.getLogger(__name__)
 class Cipher:
     def __init__(self, js: str):
         self.transform_plan: List[str] = get_transform_plan(js)
-        var_regex = re.compile(r"^\w+\W")
+        var_regex = re.compile(r"^\$*\w+\W")
         var_match = var_regex.search(self.transform_plan[0])
         if not var_match:
             raise RegexMatchError(
