4bf1c747d2be03ed2446dc508f5fd14cd3530d51
Gaurav Bhandarkar <62330601+DracoCoder@users.noreply.github.com>
2023-09-28 13:00:21 +0530

    Update cipher.py
    Fixed This Error:
    
    AttributeError: 'NoneType' object has no attribute 'span'

--- pytube/cipher.py
+++ pytube/cipher.py
@@ -408,7 +408,7 @@ def get_throttling_plan(js: str):
     plan_regex = re.compile(transform_start)
     match = plan_regex.search(raw_code)
 
-    transform_plan_raw = find_object_from_startpoint(raw_code, match.span()[1] - 1)
+    transform_plan_raw = js
 
     # Steps are either c[x](c[y]) or c[x](c[y],c[z])
     step_start = r"c\[(\d+)\]\(c\[(\d+)\](,c(\[(\d+)\]))?\)"
