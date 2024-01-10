e5476127df009bf67d1f769d47f67bbdce0d3c10
AdventurousSam <120119714+AdventurousSam@users.noreply.github.com>
2024-01-10 14:38:29 +0530

    Update captions.py
    This PR fixes the error mentioned in #1085

--- pytube/captions.py
+++ pytube/captions.py
@@ -82,15 +82,15 @@ class Caption:
             XML formatted caption tracks.
         """
         segments = []
-        root = ElementTree.fromstring(xml_captions)
+        root = ElementTree.fromstring(xml_captions).find('body')
         for i, child in enumerate(list(root)):
             text = child.text or ""
             caption = unescape(text.replace("\n", " ").replace("  ", " "),)
             try:
-                duration = float(child.attrib["dur"])
+                duration = float(child.attrib["d"])/1000
             except KeyError:
                 duration = 0.0
-            start = float(child.attrib["start"])
+            start = float(child.attrib["t"])/1000
             end = start + duration
             sequence_number = i + 1  # convert from 0-indexed to 1.
             line = "{seq}\n{start} --> {end}\n{text}\n".format(
